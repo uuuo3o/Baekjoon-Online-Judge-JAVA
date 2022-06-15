221e5870dfd2fa276121c7498a74cec36dc73e51
o3ouuu <o3ouuu@inu.ac.kr>
2022-06-15 23:08:12 +0900

    feat: Create Ex18108.java
    백준 18108번 문제 풀이

---
+++ src/Ex18108.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ex18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(year - 543);
+    }
+}
